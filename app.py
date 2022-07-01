b2d6355eb3a1dcf92018c765b30c6fdb0b3eeefe
Intraraksa <intraraksa@gmail.com>
2022-07-02 06:04:38 +0700

    change model

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import uvicorn
 from fastapi import FastAPI
 import numpy as np
@@ -7,36 +8,34 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-class request_body(BaseModel):
-    sex: str
-    age: int
-    os: str
-    friend: str
+class request_body(BaseModel): #ตั้งชื่อตัวแปร Input
+    height: float
+    weight: float
+    age: float
+    sleep: float
+    somtum : int
 
-model = pickle.load(open("socialtime_model.pickle", 'rb')) #โหลดโมเดลที่เรา Train ใน Colab
-scaler = pickle.load(open("socialtime_scaler.pickle", 'rb')) #โหลด Scaler 
-encoder = pickle.load(open("socialtime_encoder.pickle", 'rb')) #โหลด Encoder
+model = pickle.load(open("svc.pickle", 'rb')) #โหลดโมเดลที่เรา Train ใน Colab
+scaler = pickle.load(open("scaler.pickle", 'rb')) #โหลด Scaler 
 
-def pred_pipeline(inp, model=model, sc=scaler, enc=encoder): # Machine learning prediction pipeline
-    columns = ['sex', 'age', 'mobile os', 'have girl/boyfriend'] ## Column เรียงตามที่เรา Train
-    names = enc.get_feature_names([columns[0]] + columns[2:]) 
-    s = pd.DataFrame({c: [inp[i]] for i, c in enumerate(columns)})
-    encoded = enc.transform(s[[columns[0]] + columns[2:]])
-    encoded = pd.DataFrame(encoded.toarray(), columns=names)
-    x_data = pd.concat([s['age'], encoded], axis=1)
-    inp_norm = sc.transform(x_data)
-    pred = model.predict(inp_norm) 
-    return pred
+def predict_gender(question): # Pridiction Function ที่เราสร้างจาก Colab
+    data_scaled = scaler.transform([question])
+    result = model.predict(data_scaled)
+    if result==0:
+        gender = 'ชาย'
+    else:
+        gender = 'หญิง'
+    return f"คุณคือเพศ{gender}"
 
-@app.get('/')
-def index():
+@app.get('/') # หน้าแรกของ API
+def index(): 
     return {'message': 'This social time for prediction service'}
-
-@app.post("/prediction")
+ 
+@app.post("/prediction") #ส่วนของ Service Machine Learning
 def predict(data : request_body):
-    info = [data.sex,data.age,data.os,data.friend]
-    pred = pred_pipeline(info,model=model, sc=scaler, enc=encoder)
-    return {"The social time is" : f'{pred}'}
+    info = [data.height,data.weight,data.age,data.sleep,data.somtum]
+    pred = predict_gender(info)
+    return {f'{pred}'}
 
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=8000)
